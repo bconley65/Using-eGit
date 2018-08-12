0a021a294d57c2f578d6978989ff3fea615b0885
bconley65 <bconley65@hotmail.com>
2018-08-12 16:31:21 -0400

    Test 2 User Authentication

--- src/GitTutorial.java
+++ src/GitTutorial.java
@@ -2,7 +2,7 @@ public class GitTutorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to the Git Tutorial");
+		System.out.println("Welcome to this Git Tutorial");
 
 	}
 
