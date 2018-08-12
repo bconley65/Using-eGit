d3cc4a99bac85ab949bc5b3e4616572375f9eda6
bconley65 <bconley65@hotmail.com>
2018-08-12 17:18:03 -0400

    test5

--- src/GitTutorial.java
+++ src/GitTutorial.java
@@ -2,7 +2,7 @@ public class GitTutorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to this Git Tutorial");
+		System.out.println("Welcome to a Git Tutorial");
 
 	}
 
