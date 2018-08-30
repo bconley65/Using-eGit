93dc76280b02fbff649522882fd0480a83ac0e4b
Bill C <wconley@brandeis.edu>
2018-08-29 20:51:12 -0400

    This fixes an error with the login features password validation
    Testing Completed: Unit and integration tests

--- src/GitTutorial.java
+++ src/GitTutorial.java
@@ -2,7 +2,7 @@ public class GitTutorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to a Git Tutorial");
+		System.out.println("Welcome to a Git Tutorial-Committing");
 
 	}
 
